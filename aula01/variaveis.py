19304d3bd67c468b6fbca04367c143858141b0b2
samuelctl <samuel.s.aguiar@aluno.senai.br>
2025-08-07 12:09:53 -0300

    Aula do dia 07/08

---
+++ aula01/variaveis.py
@@ -0,0 +1,101 @@
+'''Exercicio: Criar um sistema com, seu nome, sua idade e data de nascimento'''
+nome = "Samuel Souza Aguiar"
+idade = 16
+nome3 = 2008
+print('Ola, me chamo', nome,"tenho", idade, "anos e nasci em ", nome3)
+'''tipos de variaveis'''
+nome = "Samuel"
+idade = 25
+altura = 1.75
+ativo = True
+print("O nome da variavel nome é: ", type(nome))
+print("O nome da variavel idade é: ", type(idade)) 
+print("O nome da variavel altura é: ", type(altura)) 
+print("O nome da variavel ativo é: ", type(ativo))
+print()
+
+
+'''print(30*"-", "operadores", 30*"-")
+#operadores
+num1 = 20
+numero2 = 4
+
+soma = num1 + numero2
+divi = num1 / numero2 # divisao comum
+resto = num1 %2
+mult = num1 * numero2
+expoente = num1 ** numero2 
+sub = num1 - numero2
+divisao_inteira = num1 // numero2 # divisao inteira
+
+print('Resultado da soma', num1, "+", numero2, "é: ", soma)
+print('Resultado da divisao', num1, "/", numero2, "é: ", divi)
+print("Resultado do resto da divisao é: ", resto)
+print('Resultado da multiplicação', num1, "*", numero2, "é: ", mult)
+print('Resultado da subtração', num1, "-", numero2, "é: ", sub)
+print('Resultado da divisao inteira', num1, "/", numero2, "é: ", divisao_inteira)
+print("Resultado do expoente é: ", expoente)
+print()
+print(30*"-", "operadores de comparação", 30*"-")
+#operadores de copmparação
+num1 > numero2 
+num1 < numero2
+num1 == numero2 
+num1 >= numero2 
+num1 <= numero2 
+num1 != numero2 
+
+ano = 2025
+print("ano atual: ", ano)
+ano += 1 
+print("ano acrecido de +1: ", ano)
+ano -= 1
+print("ano decrecido de -1: " , ano)
+
+# operadores logicos 
+# AND, OR, NOT
+
+print(30*"-", "Entrada de dados", 30*"-")
+
+nome_usario = input("Digite o seu nome: ")
+print('Seja bem-vindo ao sistema python', nome_usario)'''
+print()
+print(30*"-", "Calculadora", 30*"-")
+
+'''numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+#tipos de dados
+# float 
+soma = numero1 + numero2
+divi = numero1 / numero2 # divisao comum
+resto = numero1 %2
+mult = numero1* numero2
+expoente = numero1 ** numero2 
+sub = numero1 - numero2
+divisao_inteira = numero1 // numero2 # divisao inteira
+print('Resultado da soma', numero1, "+", numero2, "é: ", soma)
+print('Resultado da divisao', numero1, "/", numero2, "é: ", divi)
+print("Resultado do resto da divisao é: ", resto)
+print('Resultado da multiplicação', numero1, "*", numero2, "é: ", mult)
+print('Resultado da subtração', numero1, "-", numero2, "é: ", sub)
+print('Resultado da divisao inteira', numero1, "/", numero2, "é: ", divisao_inteira)
+print("Resultado do expoente é: ", expoente)
+# quandp eu passo um input ele automaticamente ele vem "str" (um texto)'''
+print()
+print(30*"-", "Convertendo tipo de dados", 30*"-")
+
+'''ano_nascimento = input("Digite seu ano de nascimento: ")
+print(type(ano_nascimento))
+# convertendo para inteiro
+ano_nascimento = int(ano_nascimento)
+print(type(ano_nascimento))
+
+saudacao = input('Digite seu nome: ')
+cpf = input('Digite seu cpf: ')
+telefone = input('Digite seu telefone: ') 
+print(20*'-', 'Dados pessoais', 20*'-')
+print("Nome: ", saudacao)
+print("Cpf: ", cpf)
+print("Telefone: ", telefone)
+# Forma de login, dados pessoais'''
+
